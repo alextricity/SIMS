56bd17d38c032047402dbd4250b686b14474ec47
alextricity <startrailsmail@gmail.com>
2018-04-23 11:04:43 -0500

    Menus Built
    Menus Built

--- MAIN.xaml.vb
+++ MAIN.xaml.vb
@@ -1,3 +1,11 @@
 ﻿Public Class MAIN
+    Private Sub Ribbon_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
 
+    End Sub
+    Sub Shutdown()
+        System.Windows.Application.Current.Shutdown()
+    End Sub
+    Sub MAIN_Closing()
+        System.Windows.Application.Current.Shutdown()
+    End Sub
 End Class
